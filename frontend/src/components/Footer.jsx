35e12b8d0d8db0426512e02846566a708b8c592b
sarthak <sarthak@leo.parth>
2026-03-03 19:14:20 +0530

    Update footer contact email

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -57,9 +57,9 @@ function Footer() {
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                                 </svg>
                                 <div className="space-y-1">
-                                    <p>Pradnya: +91 8767436959</p>
-                                    <p>Sakshi: +91 9561628406</p>
-                                    <p>Shruti: +91 8600615320</p>
+                                    <p>+91 8767436959</p>
+                                    <p>+91 9561628406</p>
+                                    <p>+91 8600615320</p>
                                 </div>
                             </div>
                             <div className="flex items-start">
